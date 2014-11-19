ef44f3b5e652fd71292bf90da1b48ade3f5f16a0
iamMarck <marck.antonio@outlook.ph>
2014-11-19 12:36:06 -0800

    firstCommit

---
+++ GitWebTest/GitWebTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitWebTest.Startup))]
+namespace GitWebTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
